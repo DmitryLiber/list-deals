c731e862eb12822232dc0ee6969e5673811d45e7
DmitryLiber <dmitry.dope@yandex.ru>
2024-02-07 18:05:58 +0300

    feat: init

---
+++ client/src/components/ui/Button.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+type TButton = {
+  loading: boolean,
+  type?: string
+  href?: string
+}
+
+export default {
+  props: {
+    href: '',
+    type: '',
+    loading: false
+  } as TButton
+}
+</script>
+
+<template>
+  <button v-if='!href' class="btn" v-bind="$attrs" >
+    <div v-if="loading" class="loader"></div>
+    <template v-else>
+      <slot />
+    </template>
+  </button>
+  <a v-else :href='href' class="btn" v-bind="$attrs" >
+    <div v-if="loading" class="loader"></div>
+    <template v-else>
+      <slot />
+    </template>
+  </a>
+</template>
+
+<style lang="scss">
+.btn {
+  background-color: cl(primary);
+}
+</style>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Button from '../components/ui/Button.vue'
+
+defineProps<{
+  href: string
+}>()
+
+</script>
+
+<template>
+  <main>
+    <Button>Кнопка</Button>
+  </main>
+</template>
